3301eb2c055cf683913b86d84d375757755e21ed
bugy <bugybon@gmail.com>
2024-11-20 19:50:48 +0200

    no json parsing

--- week07/api/eventHandler.js
+++ week07/api/eventHandler.js
@@ -1,7 +1,17 @@
 const eventHandlerRouter = require('express').Router();
 
-const dataBase = {};
+const dataBase = new Map();
 
-eventHandlerRouter.post('/event/', (req, res) =>{
-    
+eventHandlerRouter.post('/event', (req, res) =>{
+    const name = req.body.name;
+    const capacity = req.body.capacity;
+    const newEvent = {name: name, capacity: capacity};
+    dataBase.set(dataBase.size, newEvent);
+    res.send(newEvent);
 });
+
+eventHandlerRouter.get('/event/:id', (req,res) =>{
+
+});
+
+module.exports = eventHandlerRouter;
