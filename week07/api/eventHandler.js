900a497640ba33ae242bc1f25ebda907a39a0330
bugy <bugybon@gmail.com>
2024-11-27 20:01:32 +0200

    Id now is not related to db size + db has given id check added

--- week07/api/eventHandler.js
+++ week07/api/eventHandler.js
@@ -2,20 +2,24 @@ const eventHandlerRouter = require('express').Router();
 
 const dataBase = new Map();
 const bookingDataBase = new Map();
+let eventID = 0;
 
 eventHandlerRouter.post('/', (req, res) =>{
     const name = req.body.name;
     const capacity = req.body.capacity;
     const newEvent = {name: name, capacity: capacity};
-    dataBase.set(dataBase.size, newEvent);
-    bookingDataBase.set(bookingDataBase.size, new Map());
+    dataBase.set(eventID++, newEvent);
+    bookingDataBase.set(bookingDataBase.size, {dataBase:new Map(), id:0});
     res.send(newEvent);
     // const str = req.body;
     // console.log(str);
 });
 
 eventHandlerRouter.get('/:id', (req,res) =>{
-    res.send(dataBase.get(+req.params.id));
+    if(!dataBase.has(+req.params.id)){
+        res.send("Event with this Id doesn't exist");
+    }else
+        res.send(dataBase.get(+req.params.id));
 });
 
 eventHandlerRouter.delete('/:id', (req,res) =>{
@@ -30,9 +34,9 @@ eventHandlerRouter.post('/:id/booking', (req, res) =>{
     const newBooking = { firstName: firstName, lastName: lastName };
     const currentBooking = bookingDataBase.get(+req.params.id);
     const currentEvent = dataBase.get(+req.params.id);
-    if(currentBooking.size < currentEvent.capacity){
-        currentBooking.set(currentBooking.size, newBooking);
-        res.send("".concat(currentEvent.capacity - currentBooking.size));
+    if(currentBooking.dataBase.size < currentEvent.capacity){
+        currentBooking.dataBase.set(currentBooking.id++, newBooking);
+        res.send("".concat(currentEvent.capacity - currentBooking.dataBase.size));
     }else{
         res.send("No available spaces");
     }
@@ -44,7 +48,7 @@ eventHandlerRouter.post('/:id/booking', (req, res) =>{
 eventHandlerRouter.get('/:id/booking', (req,res) =>{
     const currentBooking = bookingDataBase.get(+req.params.id);
     let result = "";
-    for(booking of currentBooking){
+    for(booking of currentBooking.dataBase){
         result = result.concat(booking[1].firstName,' ', booking[1].lastName, '\n');
     }
     res.send(result);
@@ -52,12 +56,12 @@ eventHandlerRouter.get('/:id/booking', (req,res) =>{
 
 eventHandlerRouter.get('/:id/booking/:bookingId', (req,res) =>{
     const currentBooking = bookingDataBase.get(+req.params.id);
-    res.send(currentBooking.get(+req.params.bookingId));
+    res.send(currentBooking.dataBase.get(+req.params.bookingId));
 });
 
 eventHandlerRouter.delete('/:id/booking/:bookingId', (req,res) =>{
     const currentBooking = bookingDataBase.get(+req.params.id);
-    currentBooking.delete(+req.params.bookingId);
+    currentBooking.dataBase.delete(+req.params.bookingId);
     res.send('Booking deleted');
 });
 
